ebc79b6ae85be93642da9b8961821b9b23eb5cba
Matheus Cunha Nicolini <cunhanicolini@hotmail.com>
2019-09-20 17:00:12 -0300

    Fazendo conexão com o socket.io

--- src/server.js
+++ src/server.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const routes = require('./routes');
 
+// Passa o express para o http-server
+const http = require('http').Server(routes);
+
+// Passa o http-server par ao socket.io
+const io = require('socket.io')(http);
+
 const server = express();
 
 server.use(express.json());
 server.use(routes);
 
-server.listen(8080);
+io.on('connection', (socket) => {
+  console.log('Usuário conectado');
+  socket.on('disconnect', () => {
+    console.log('Usuário desconectado');
+  });
+  socket.on('chat message', function(msg){
+    console.log('Mensagem: ' + msg);
+  });
+});
+
+// Inicia o servidor na porta informada, no caso vamo iniciar na porta 8080
+http.listen(8080, () => {
+  console.log('Servidor rodando em: http://localhost:8080');
+});
+
+// server.listen(8080);
+
